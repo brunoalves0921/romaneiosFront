8956a3c76ccdbdff97d6dd6188c07b297a869a05
bruno alves <jorge.bruno0921@alu.ufc.br>
2024-01-19 14:38:43 -0300

    update

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,12 @@
 import axios from 'axios'
+import { Agent } from 'https'
+
 
 const api = axios.create({
     baseURL: process.env.API_URL ?? process.env.NEXT_PUBLIC_API_URL,
+    httpsAgent: new Agent({
+        rejectUnauthorized: false
+    })
 })
 
 export default api
